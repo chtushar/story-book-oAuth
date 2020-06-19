c9a04223624f80d9cc5813fc627b047c7061cc58
Tushar Choudhari <choudharit23@gmail.com>
2020-06-20 02:22:39 +0530

    passport setup

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const connectDB = require("./db/mongoose");
+const passport = require("passport");
+const session = require("express-session");
 
+//Load config
 dotenv.config({ path: "./config/config.env" });
+
+//passport config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -20,11 +26,25 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+//sessions
+app.use(
+  session({
+    secret: "xyztush",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
